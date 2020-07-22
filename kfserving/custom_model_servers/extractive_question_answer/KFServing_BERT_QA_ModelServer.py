86ea1c8b89daaffac63d8f032b9a09526853b754
Josh Patterson <josh@Joshs-iMac.local>
2020-07-22 11:09:04 -0400

    de-gunked comments out

--- kfserving/custom_model_servers/extractive_question_answer/KFServing_BERT_QA_ModelServer.py
+++ kfserving/custom_model_servers/extractive_question_answer/KFServing_BERT_QA_ModelServer.py
@@ -24,44 +24,13 @@ class KFServing_BERT_QA_Model(kfserving.KFModel):
         self.tokenizer = None
 
     def load(self):
-        #f = open('imagenet_classes.txt')
-        #self.classes = [line.strip() for line in f.readlines()]
-
-        #model = models.alexnet(pretrained=True)
-        #model.eval()
-        #self.model = model
 
         self.tokenizer = AutoTokenizer.from_pretrained("bert-large-uncased-whole-word-masking-finetuned-squad")
         self.model = TFAutoModelForQuestionAnswering.from_pretrained("bert-large-uncased-whole-word-masking-finetuned-squad")
-
-
-
-
-
         self.ready = True
 
     def predict(self, request: Dict) -> Dict:
         inputs = request["instances"]
-
-        # Input follows the Tensorflow V1 HTTP API for binary values
-        # https://www.tensorflow.org/tfx/serving/api_rest#encoding_binary_values
-        """
-        data = inputs[0]["image"]["b64"]
-
-        raw_img_data = base64.b64decode(data)
-        input_image = Image.open(io.BytesIO(raw_img_data))
-
-        preprocess = transforms.Compose([
-            transforms.Resize(256),
-            transforms.CenterCrop(224),
-            transforms.ToTensor(),
-            transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                                 std=[0.229, 0.224, 0.225]),
-        ])
-
-        input_tensor = preprocess(input_image)
-        input_batch = input_tensor.unsqueeze(0)
-        """
 
         source_text = inputs[0]["text"]
         questions = inputs[0]["questions"]
@@ -93,16 +62,6 @@ class KFServing_BERT_QA_Model(kfserving.KFModel):
 
             results[ question ] = answer
 
-
-        #output = self.model(input_batch)
-
-        #scores = torch.nn.functional.softmax(output, dim=1)[0]
-
-        #_, top_5 = torch.topk(output, 5)
-
-        #for idx in top_5[0]:
-        #    results[self.classes[idx]] = scores[idx].item()
-        
 
         return {"predictions": results}
 
